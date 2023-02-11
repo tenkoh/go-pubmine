359e6466c02b3c2e363b84b7e3b80afbbf09c007
tenkoh <tenkoh.go@gmail.com>
2023-02-12 06:33:33 +0900

    trial: basic view and trial wasm function

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 //go:build js && wasm
-// +build js,wasm
 
 package main
 
 //go:generate cp $GOROOT/misc/wasm/wasm_exec.js .
 
-func main() {
+import (
+	"syscall/js"
+)
 
+func main() {
+	doc := js.Global().Get("document")
+	input := doc.Call("getElementById", "input")
+	prefix := input.Get("value").String()
+
+	public := doc.Call("getElementById", "public")
+	public.Set("value", prefix)
+	private := doc.Call("getElementById", "private")
+	private.Set("value", prefix)
 }
